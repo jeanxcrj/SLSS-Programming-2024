0cb034b3e76dcc8448eaa99194cfdfa4910f89ef
Jean Chen <jeanchen.crjj@gmail.com>
2024-04-05 14:35:44 -0700

    added more notes

--- notes-2-2-morefunctions.py
+++ notes-2-2-morefunctions.py
@@ -3,3 +3,28 @@
 #4 April 2024
 
 
+#Multiply strings 
+greeting = "hello"
+print(greeting * 2)
+
+
+print("The quick brown fox jumps over the lazy dog." * 2)
+
+def stars(num_stars: int) -> str:
+    """Returns a given number of *"""
+    value = " "
+    # If value == 0 or value == 1 "*"
+    # if value > 1 "*" * num_stars
+    # else negative number -> error 
+    if num_stars == 0 or num_stars == 1:
+        value = "*"
+    elif num_stars > 1:
+        value = "*" * num_stars
+    else:
+        value = "Sorry, can't take negative numbers"
+    return value
+
+print(stars(1))
+print(stars(100))
+print(stars(0))
+print(stars)
